752d1e87a919809234909ddff514d1ffdf31ebf3
Ojas Abhay Sonnis <51595734+ojassonnis@users.noreply.github.com>
2020-02-28 19:10:37 -0600

    Add files via upload

---
+++ testinstallojso.py
@@ -0,0 +1,3 @@
+user_name = input('Please enter your full name separated by spaces only: ')
+len_name = len(user_name)
+print('Hello {}. Your name contains {} characters'.format(user_name, len_name))
